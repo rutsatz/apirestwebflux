d250fa9012414739e34fc302ee154cb84834cbd5
Rafael Fernando Rutsatz <rafael.rutsatz@compasso.com.br>
2020-04-13 15:50:08 -0300

    Adicionado stream de eventos.

--- src/main/java/com/apirest/webflux/PlaylistRouter.java
+++ src/main/java/com/apirest/webflux/PlaylistRouter.java
@@ -1,25 +1,25 @@
-package com.apirest.webflux;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.RequestPredicates;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
-
-@Configuration
-public class PlaylistRouter {
-
-	@Bean
-	public RouterFunction<ServerResponse> route(PlaylistHandler handler) {
-		return RouterFunctions//
-				.route(RequestPredicates.GET("/playlist").and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
-						handler::findAll)
-				.andRoute(RequestPredicates.GET("/playlist/{id}")
-						.and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), handler::findById)
-				.andRoute(RequestPredicates.POST("/playlist").and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
-						handler::save);
-	}
-
-}
+//package com.apirest.webflux;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.reactive.function.server.RequestPredicates;
+//import org.springframework.web.reactive.function.server.RouterFunction;
+//import org.springframework.web.reactive.function.server.RouterFunctions;
+//import org.springframework.web.reactive.function.server.ServerResponse;
+//
+//@Configuration
+//public class PlaylistRouter {
+//
+//	@Bean
+//	public RouterFunction<ServerResponse> route(PlaylistHandler handler) {
+//		return RouterFunctions//
+//				.route(RequestPredicates.GET("/playlist").and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
+//						handler::findAll)
+//				.andRoute(RequestPredicates.GET("/playlist/{id}")
+//						.and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), handler::findById)
+//				.andRoute(RequestPredicates.POST("/playlist").and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
+//						handler::save);
+//	}
+//
+//}
--- src/main/java/com/apirest/webflux/controller/PlaylistController.java
+++ src/main/java/com/apirest/webflux/controller/PlaylistController.java
@@ -1,6 +1,9 @@
 package com.apirest.webflux.controller;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,8 +15,9 @@ import com.apirest.webflux.service.PlaylistService;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
-//@RestController
+@RestController
 public class PlaylistController {
 
 	@Autowired
@@ -32,6 +36,18 @@ public class PlaylistController {
 	@PostMapping("playlist")
 	public Mono<Playlist> save(@RequestBody Playlist playlist) {
 		return playlistService.save(playlist);
+	}
+
+	@GetMapping(value = "/playlist/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<Tuple2<Long, Playlist>> getPlaylistByEvents() {
+		/*
+		 * Intervalo de cada stream que será enviado ao cliente. Utiliza o interval do
+		 * Flux. Usa o Duration para definir o intervalo de 5 segundos.
+		 */
+		Flux<Long> interval = Flux.interval(Duration.ofSeconds(5));
+		Flux<Playlist> events = playlistService.findAll();
+		System.out.println("####### Passou aqui events");
+		return Flux.zip(interval, events);
 	}
 
 }
